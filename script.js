ab510ff0040ac6317c9c1410bf0c12acad8479b6
coolguy-codin-Rydham <rydhampreetsingh.gindra@gmail.com>
2024-01-17 19:12:46 +0530

    Used Foreach this time

--- script.js
+++ script.js
@@ -7,22 +7,29 @@ let herobg=document.getElementById('herobg')
 
 // console.log(btns[0]);
 
+// let btns = document.querySelectorAll('.shoes');
+// let main = document.querySelector('#MainShoe');
+// let herobg = document.getElementById('herobg');
 
-btns[0].addEventListener('click', ()=>{
-    main.src=`./assets/air-max-2x-shoe-Wh9d6P-removebg-preview 1.svg`;
-    main.classList.add('rotate20')
-    herobg.style.backgroundImage="url('./assets/JUSTDOITBlack.svg')"
-    // main.classList.remove('right-10');
-    // main.classList.add('right-52');
-})
-
-btns[1].addEventListener('click',()=>{
-    main.src='./assets/air-max-2x-shoe-Wh9d6P-removebg-preview 2.svg';
-    herobg.style.backgroundImage="url('./assets/JustDoItPink.svg')"
-    main.classList.add('rotate20')
-})
-btns[2].addEventListener('click', ()=>{
-    main.src=`./assets/Shoe 3.svg`;
-    main.classList.remove('rotate20')
-    herobg.style.backgroundImage="url('./assets/JUST DO IT (1).svg')"
-})
+btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        switch (index) {
+            case 0:
+                main.src = './Assets/air-max-2x-shoe-Wh9d6P-removebg-preview 1.svg';
+                main.classList.add('rotate20');
+                herobg.style.backgroundImage = "url('./Assets/JUSTDOITBlack.svg')";
+                break;
+            case 1:
+                main.src = './Assets/air-max-2x-shoe-Wh9d6P-removebg-preview 2.svg';
+                herobg.style.backgroundImage = "url('./Assets/JustDoItPink.svg')";
+                main.classList.add('rotate20');
+                break;
+            case 2:
+                main.src = './Assets/Shoe 3.svg';
+                main.classList.remove('rotate20');
+                herobg.style.backgroundImage = "url('./Assets/JUST DO IT (1).svg')";
+                break;
+            // Add more cases if needed
+        }
+    });
+});
