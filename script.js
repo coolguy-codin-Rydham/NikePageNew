2f65f84d071dc9663cb3b1dfd3e9cbb1a61a203a
coolguy-codin-Rydham <rydhampreetsingh.gindra@gmail.com>
2024-01-17 19:19:46 +0530

    Just Why

--- script.js
+++ script.js
@@ -11,25 +11,22 @@ let herobg=document.getElementById('herobg')
 // let main = document.querySelector('#MainShoe');
 // let herobg = document.getElementById('herobg');
 
-btns.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        switch (index) {
-            case 0:
-                main.src = './Assets/air-max-2x-shoe-Wh9d6P-removebg-preview 1.svg';
-                main.classList.add('rotate20');
-                herobg.style.backgroundImage = "url('./Assets/JUSTDOITBlack.svg')";
-                break;
-            case 1:
-                main.src = './Assets/air-max-2x-shoe-Wh9d6P-removebg-preview 2.svg';
-                herobg.style.backgroundImage = "url('./Assets/JustDoItPink.svg')";
-                main.classList.add('rotate20');
-                break;
-            case 2:
-                main.src = './Assets/Shoe 3.svg';
-                main.classList.remove('rotate20');
-                herobg.style.backgroundImage = "url('./Assets/JUST DO IT (1).svg')";
-                break;
-            // Add more cases if needed
-        }
-    });
-});
+
+btns[0].addEventListener('click', ()=>{
+    main.src=`./Assets/air-max-2x-shoe-Wh9d6P-removebg-preview 1.svg`;
+    main.classList.add('rotate20')
+    herobg.style.backgroundImage="url('./Assets/JUSTDOITBlack.svg')"
+    // main.classList.remove('right-10');
+    // main.classList.add('right-52');
+})
+
+btns[1].addEventListener('click',()=>{
+    main.src='/Assets/air-max-2x-shoe-Wh9d6P-removebg-preview 2.svg';
+    herobg.style.backgroundImage="url('./Assets/JustDoItPink.svg')"
+    main.classList.add('rotate20')
+})
+btns[2].addEventListener('click', ()=>{
+    main.src=`./Assets/Shoe 3.svg`;
+    main.classList.remove('rotate20')
+    herobg.style.backgroundImage="url('./Assets/JUST DO IT (1).svg')"
+})
