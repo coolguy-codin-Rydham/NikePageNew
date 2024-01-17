3f47c65a710316c55f61104f1a3d206c8b5aff7e
coolguy-codin-Rydham <rydhampreetsingh.gindra@gmail.com>
2024-01-17 19:06:02 +0530

    Nike Page with JS

---
+++ script.js
@@ -0,0 +1,28 @@
+let btns= document.querySelectorAll('.shoes');
+
+let main=document.querySelector('#MainShoe')
+
+let herobg=document.getElementById('herobg')
+
+
+// console.log(btns[0]);
+
+
+btns[0].addEventListener('click', ()=>{
+    main.src=`./assets/air-max-2x-shoe-Wh9d6P-removebg-preview 1.svg`;
+    main.classList.add('rotate20')
+    herobg.style.backgroundImage="url('./assets/JUSTDOITBlack.svg')"
+    // main.classList.remove('right-10');
+    // main.classList.add('right-52');
+})
+
+btns[1].addEventListener('click',()=>{
+    main.src='./assets/air-max-2x-shoe-Wh9d6P-removebg-preview 2.svg';
+    herobg.style.backgroundImage="url('./assets/JustDoItPink.svg')"
+    main.classList.add('rotate20')
+})
+btns[2].addEventListener('click', ()=>{
+    main.src=`./assets/Shoe 3.svg`;
+    main.classList.remove('rotate20')
+    herobg.style.backgroundImage="url('./assets/JUST DO IT (1).svg')"
+})
